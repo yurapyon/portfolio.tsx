25c8b91c6c81872cab5c236d096d9dc1bdcf7a6e
extrasharp <genericpb@gmail.com>
2021-09-03 21:34:35 -0500

    actually make sure it builds

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,18 @@ interface Entry {
   name: string,
   link: string,
   description: string,
-  long_info?: string,
-  images?: [string],
+  long_info?: any,
+  images?: string[],
 }
 
 const renderEntry = (e: Entry) => {
   return (
     <>
-      <h2><a href={e.link} target="_blank">{e.name}</a></h2>
+      <h2><a href={e.link} target="_blank" rel="noreferrer">{e.name}</a></h2>
       <p>{e.description}</p>
       {e.long_info ? e.long_info() : " "}
       {e.images ? e.images.map((file) => {
-        return <img src={file} width={700}></img>
+        return <img src={file} width={700} key={file}></img>
       }) : " " }
     </>
   );
@@ -29,13 +29,13 @@ const entries = [
     name: "maru",
     link: "https://github.com/yurapyon/maru",
     description: "OpenGL game/graphics framework",
-    long_info: () => {
+    long_info: function maruInfo() {
       return (
       <p>
         maru is a graphics framework designed to make it easy to get up
         and running with motion graphics or game projects.
         A video made with an older version of the software can be found here: {" "}
-        <a href ="https://www.youtube.com/watch?v=loHvrW5EKBk" style={{color:"#222288"}} target="_blank">
+        <a href ="https://www.youtube.com/watch?v=loHvrW5EKBk" style={{color:"#222288"}} target="_blank" rel="noreferrer">
           https://www.youtube.com/watch?v=loHvrW5EKBk
         </a>
       </p>
@@ -46,7 +46,7 @@ const entries = [
     name: "uokichi",
     link: "https://github.com/yurapyon/uokichi-scm",
     description: "AVR assembler",
-    long_info: () => {
+    long_info: function uoInfo() {
       return (
       <p>
         uokichi is an assember, programmer, and uploader for AVR microcontrollers.
@@ -60,7 +60,7 @@ const entries = [
     name: "kasumi",
     link: "https://github.com/yurapyon/kasumi",
     description: "audio framework",
-    long_info: () => {
+    long_info: function kasumiInfo() {
       return (
       <p>
         kasumi is a realtime audio engine for use in games and digital audio workstations.
@@ -74,7 +74,7 @@ const entries = [
     name: "The Worst Compilation: Forth Wave",
     link: "https://theworstlabel.bandcamp.com/album/the-worst-compilation-fourth-wave",
     description: "Covers, inlays and CD art",
-    long_info: () => {
+    long_info: function worstInfo () {
       return <p>Tools used: Photoshop, Illustrator, Blender</p>
     },
     images: [
@@ -86,7 +86,7 @@ const entries = [
     name: "Angel93",
     link: "https://cockrockdisco.bandcamp.com/album/angel-93/",
     description: "Covers",
-    long_info: () => {
+    long_info: function angel93Info() {
       return <p>Tools used: Photoshop, Blender</p>
     },
     images: [
@@ -99,7 +99,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>mel aise >> portfolio</title>
+        <title>mel aise {">>"} portfolio</title>
         <meta name="description" content="" />
       </Head>
 
@@ -109,11 +109,11 @@ const Home: NextPage = () => {
           <img src="/content/ap.jpg" width={600}></img>
         </p>
         <p>
-          <a href="https://www.twitter.com/drinkdnL" target="_blank">[tw]</a>
-          <a href="https://www.soundcloud.com/weoooweooo" target="_blank">[sc]</a>
-          <a href="https://www.github.com/extrasharp" target="_blank">[gh]</a>
+          <a href="https://www.twitter.com/drinkdnL" target="_blank" rel="noreferrer">[tw]</a>
+          <a href="https://www.soundcloud.com/weoooweooo" target="_blank" rel="noreferrer">[sc]</a>
+          <a href="https://www.github.com/extrasharp" target="_blank" rel="noreferrer">[gh]</a>
           {" "}
-          <a href="content/resume.pdf" target="_blank">résumé</a>
+          <a href="content/resume.pdf" target="_blank" rel="noreferrer">résumé</a>
         </p>
       </div>
 
